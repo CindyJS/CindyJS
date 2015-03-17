1ff1f0916d96eab91e0336aa98c807ba8d0652e1
Martin von Gagern <gagern@ma.tum.de>
2015-03-17 20:25:06 +0100

    Beautify.

--- src/js/libgeo/GeoOps.js
+++ src/js/libgeo/GeoOps.js
@@ -520,16 +520,23 @@ geoOps._helper.ConicBy3p2l = function(a, b, c, g, h) {
     var m2 = List.adjoint3(List.turnIntoCSList([
         List.scalmult(s1.value[0], gl),
         List.scalmult(s1.value[1], hl),
-        List.scalmult(s1.value[2], gh)]));
+        List.scalmult(s1.value[2], gh)
+    ]));
     var m3 = List.transpose(m2);
     var mul = CSNumber.mult;
-    var aa = List.productMV(m3, a), a1 = aa.value[0], a2 = aa.value[1];
-    var bb = List.productMV(m3, b), b1 = bb.value[0], b2 = bb.value[1];
+    var aa = List.productMV(m3, a);
+    var a1 = aa.value[0];
+    var a2 = aa.value[1];
+    var bb = List.productMV(m3, b);
+    var b1 = bb.value[0];
+    var b2 = bb.value[1];
     // assert: aa.value[2] and bb.value[2] are zero
-    var a3a = CSNumber.sqrt(mul(a1, a2)), b3a = CSNumber.sqrt(mul(b1, b2));
+    var a3a = CSNumber.sqrt(mul(a1, a2));
+    var b3a = CSNumber.sqrt(mul(b1, b2));
     var signs, res = Array(4);
     for (signs = 0; signs < 4; ++signs) {
-        var sa = ((signs & 1) << 1) - 1, sb = (signs & 2) - 1;
+        var sa = ((signs & 1) << 1) - 1;
+        var sb = (signs & 2) - 1;
         var a3 = mul(CSNumber.real(sa), a3a);
         var b3 = mul(CSNumber.real(sb), b3a);
         var p1 = det2(a2, a3, b2, b3);
@@ -540,19 +547,24 @@ geoOps._helper.ConicBy3p2l = function(a, b, c, g, h) {
                 det2(b1, b2, a1, a2),
                 det2(b2, b3, a2, a3)),
             det2(b3, b1, a3, a1));
-        var xx = mul(p1, p1), yy = mul(p2, p2), zz = mul(p4, p4);
-        var xy = mul(p1, p2), xz = mul(p1, p4), yz = mul(p2, p4);
+        var xx = mul(p1, p1);
+        var yy = mul(p2, p2);
+        var zz = mul(p4, p4);
+        var xy = mul(p1, p2);
+        var xz = mul(p1, p4);
+        var yz = mul(p2, p4);
         xy = CSNumber.sub(xy, mul(CSNumber.real(0.5), mul(p3, p3)));
         var mm = List.turnIntoCSList([
             List.turnIntoCSList([xx, xy, xz]),
             List.turnIntoCSList([xy, yy, yz]),
             List.turnIntoCSList([xz, yz, zz])
         ]);
-        var cc = List.realVector([1,1,1]);
+        var cc = List.realVector([1, 1, 1]);
         mm = List.productMM(m2, List.productMM(mm, m3));
         res[signs] = mm;
     }
     return res;
+
     function det2(a, b, c, d) {
         return CSNumber.sub(CSNumber.mult(a, d), CSNumber.mult(b, c));
     }
