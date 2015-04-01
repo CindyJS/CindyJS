86cc315b1e533077e88cd071d37a993f47393043
Martin von Gagern <gagern@ma.tum.de>
2015-04-01 21:34:34 +0200

    More careful HiDPI handling.
    Avoid referencing the window object from node tests.
    Avoid overwriting style properties that might already be set.

--- src/js/Setup.js
+++ src/js/Setup.js
@@ -124,7 +124,9 @@ function createCindyNow() {
         csh = c.height;
         csport.drawingstate.matrix.ty = csport.drawingstate.matrix.ty - csh;
         csport.drawingstate.initialmatrix.ty = csport.drawingstate.initialmatrix.ty - csh;
-        var devicePixelRatio = window.devicePixelRatio || 1;
+        var devicePixelRatio = 1;
+        if (typeof window !== "undefined" && window.devicePixelRatio)
+            devicePixelRatio = window.devicePixelRatio;
         var backingStoreRatio =
             csctx.webkitBackingStorePixelRatio ||
             csctx.mozBackingStorePixelRatio ||
@@ -136,8 +138,10 @@ function createCindyNow() {
             var ratio = devicePixelRatio / backingStoreRatio;
             c.width = csw * ratio;
             c.height = csh * ratio;
-            c.style.width = csw + "px";
-            c.style.height = csh + "px";
+            if (!c.style.width)
+                c.style.width = csw + "px";
+            if (!c.style.height)
+                c.style.height = csh + "px";
             csctx.scale(ratio, ratio);
         }
     }
