754f6a3b12af96855b0bb48e640eaa8181544d3a
Martin von Gagern <gagern@ma.tum.de>
2015-04-16 17:03:25 +0200

    Reduce indentation using continue

--- src/js/Events.js
+++ src/js/Events.js
@@ -49,55 +49,55 @@ function getmover(mouse) {
     var diff;
     for (var i = 0; i < csgeo.free.length; i++) {
         var el = csgeo.free[i];
-        if (!el.pinned) {
+        if (el.pinned)
+            continue;
 
-            var dx, dy, dist;
-            var sc = csport.drawingstate.matrix.sdet;
-            if (el.kind === "P") {
-                dx = el.sx - mouse.x;
-                dy = el.sy - mouse.y;
-                dist = Math.sqrt(dx * dx + dy * dy);
-                if (el.narrow & dist > 20 / sc) dist = 10000;
-            } else if (el.kind === "C") { //Must be Circle by Rad
-                var mid = csgeo.csnames[el.args[0]];
-                var rad = el.radius;
-                var xx = CSNumber.div(mid.homog.value[0], mid.homog.value[2]).value.real;
-                var yy = CSNumber.div(mid.homog.value[1], mid.homog.value[2]).value.real;
-                dx = xx - mouse.x;
-                dy = yy - mouse.y;
-                var ref = Math.sqrt(dx * dx + dy * dy);
-                dist = ref - rad.value.real;
-                dx = 0;
-                dy = 0;
-                if (dist < 0) {
-                    dist = -dist;
-                }
-                dist = dist + 30 / sc;
-
-            } else if (el.kind === "L") { //Must be ThroughPoint(Horizontal/Vertical not treated yet)
-                var l = el.homog;
-                var N = CSNumber;
-                var nn = N.add(N.mult(l.value[0], N.conjugate(l.value[0])),
-                    N.mult(l.value[1], N.conjugate(l.value[1])));
-                var ln = List.scaldiv(N.sqrt(nn), l);
-                dist = ln.value[0].value.real * mouse.x + ln.value[1].value.real * mouse.y + ln.value[2].value.real;
-                dx = ln.value[0].value.real * dist;
-                dy = ln.value[1].value.real * dist;
-
-                if (dist < 0) {
-                    dist = -dist;
-                }
-                dist = dist + 1;
+        var dx, dy, dist;
+        var sc = csport.drawingstate.matrix.sdet;
+        if (el.kind === "P") {
+            dx = el.sx - mouse.x;
+            dy = el.sy - mouse.y;
+            dist = Math.sqrt(dx * dx + dy * dy);
+            if (el.narrow & dist > 20 / sc) dist = 10000;
+        } else if (el.kind === "C") { //Must be Circle by Rad
+            var mid = csgeo.csnames[el.args[0]];
+            var rad = el.radius;
+            var xx = CSNumber.div(mid.homog.value[0], mid.homog.value[2]).value.real;
+            var yy = CSNumber.div(mid.homog.value[1], mid.homog.value[2]).value.real;
+            dx = xx - mouse.x;
+            dy = yy - mouse.y;
+            var ref = Math.sqrt(dx * dx + dy * dy);
+            dist = ref - rad.value.real;
+            dx = 0;
+            dy = 0;
+            if (dist < 0) {
+                dist = -dist;
             }
+            dist = dist + 30 / sc;
 
-            if (dist < adist + 0.2 / sc) { //A bit a dirty hack, prefers new points
-                adist = dist;
-                mov = el;
-                diff = {
-                    x: dx,
-                    y: dy
-                };
+        } else if (el.kind === "L") { //Must be ThroughPoint(Horizontal/Vertical not treated yet)
+            var l = el.homog;
+            var N = CSNumber;
+            var nn = N.add(N.mult(l.value[0], N.conjugate(l.value[0])),
+                N.mult(l.value[1], N.conjugate(l.value[1])));
+            var ln = List.scaldiv(N.sqrt(nn), l);
+            dist = ln.value[0].value.real * mouse.x + ln.value[1].value.real * mouse.y + ln.value[2].value.real;
+            dx = ln.value[0].value.real * dist;
+            dy = ln.value[1].value.real * dist;
+
+            if (dist < 0) {
+                dist = -dist;
             }
+            dist = dist + 1;
+        }
+
+        if (dist < adist + 0.2 / sc) { //A bit a dirty hack, prefers new points
+            adist = dist;
+            mov = el;
+            diff = {
+                x: dx,
+                y: dy
+            };
         }
     }
     console.log("Moving " + mov.name);
