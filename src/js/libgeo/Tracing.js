33ca2df95858dd3056e9a2a3db5b7cc0835bbb64
Martin von Gagern <gagern@ma.tum.de>
2015-04-25 02:39:49 +0200

    Move tracing code to Tracing.js

--- src/js/libgeo/Tracing.js
+++ src/js/libgeo/Tracing.js
@@ -1,3 +1,280 @@
+/* Thoughts about state handling.
+ * - trace has to start from stateLastGood
+ * - each trace step reads from stateIn and writes to stateOut
+ * - if refinement is requested, we have to keep stateIn
+ * - if a step is concluded (succeeded or failed, no refinement),
+ *   then we want to use stateOut as the next stateIn
+ *   and the intermediate parameter as the next start parameter
+ * - after a whole trace run, if no step failed, stateOut
+ *   becomes stateLastGood since everything is good
+ * - on mouseup or after recalc, our stateOut becomes stateLastGood
+ * - stateIn and stateLastGood sometimes refer to the same object
+ * - we need at least three states and four variables for all of this
+ * - we try to avoid copying data between arrays, and swap arrays instead
+ */
+
+/* Details on state handling:
+ * Between traces, we have two possible situations.
+ *
+ * A) The current state is good.
+ *    In this case, stateIn and stateLastGood point to the same array,
+ *    representing the current state. stateOut and stateSpare point to
+ *    two distinct arrays, the content of which is irrelevant.
+ *
+ * B) The current state is bad.
+ *    In this case, stateIn points to the current state, and stateLastGood
+ *    points to the last state which has been considered good. stateOut
+ *    points to a third array with irrelevant content. stateSpare is null.
+ *
+ * There are a number of transition functions to switch between these
+ * situations. These are called AFTER a tracing or recalc run, so that
+ * things are already prepared for the next run in the way described
+ * above.
+ */
+
+var stateIn, stateOut, stateLastGood, stateSpare;
+
+/**
+ * stateOut is a good state, make it the last good state as well as
+ * the next stateIn. Ensure we don't loose any buffers.
+ */
+function stateSwapGood() {
+    var resultState = stateOut;
+    stateOut = stateIn;
+    stateSpare = stateSpare || stateLastGood;
+    stateLastGood = stateIn = resultState;
+}
+
+/**
+ * stateOut is a bad (failed or incomplete) state, make it the next stateIn but
+ * keep the current stateLastGood. Ensure we don't loose any buffers.
+ */
+function stateSwapBad() {
+    var resultState = stateOut;
+    stateOut = stateSpare || stateIn;
+    stateSpare = null;
+    stateIn = resultState;
+}
+
+/**
+ * Current state (i.e. stateIn) is now deemed good, even in case it
+ * wasn't considered good before. Make it the stateLastGood. If we
+ * were in a good situation, there is nothing to do.
+ */
+function stateContinueFromHere() {
+    if (!stateSpare) {
+        stateSpare = stateLastGood;
+        stateLastGood = stateIn;
+        tracingStateReport(false);
+    }
+}
+
+/**
+ * Make stateIn point to the last good state.
+ * If it already does, there is nothing to do.
+ */
+function stateRevertToGood() {
+    if (!stateSpare) {
+        stateSpare = stateIn;
+        stateIn = stateLastGood;
+    }
+}
+
+var stateInIdx, stateOutIdx;
+
+var tracingInitial, tracingFailed, noMoreRefinements;
+
+var RefineException = {
+    toString: function() {
+        return "RefineException";
+    }
+};
+
+function requestRefinement() {
+    // Call this whenever you would need exra refinement.
+    // Possible outcomes: either an exception will be thrown to
+    // request more refinements, or the tracingFailed flag will be set
+    // and the function returns normally.
+    if (noMoreRefinements) tracingFailed = true;
+    else throw RefineException;
+}
+
+function defaultParameterPath(el, tr, tc, src, dst) {
+    // src + t * (dst - src)
+    el.param = General.add(src, General.mult(tc, General.sub(dst, src)));
+}
+
+function trace() {
+    //console.log(stateLastGood);
+    var mover = move.mover;
+    var deps = getGeoDependants(mover);
+    var last = -1;
+    var t = 1;
+    var step = 2;
+    var i, el, op;
+    var opMover = geoOps[mover.type];
+    var parameterPath = opMover.parameterPath || defaultParameterPath;
+    stateRevertToGood();
+    var lastGoodParam = move.lastGoodParam;
+    try {
+        opMover.computeParametersOnInput(mover, lastGoodParam);
+    } catch (e) {
+        if (e !== RefineException)
+            throw e;
+        // The parameter computation may explicitely forbid a
+        // single-step interpolation. PointOnCircle does that.
+        step = 0.55; // will take three steps if all goes well
+        t = last + step;
+    }
+    var targetParam = mover.param; // not cloning, must not get modified
+    tracingFailed = false;
+    while (last !== t) {
+        if (traceLog) {
+            traceLogRow = [];
+            traceLog.push(traceLogRow);
+            if (last === -1 && t === 1) {
+                traceLogRow[0] = niceprint(lastGoodParam);
+                traceLogRow[1] = niceprint(targetParam);
+            } else {
+                traceLogRow[0] = "";
+                traceLogRow[1] = "";
+            }
+            traceLogRow[2] = last;
+            traceLogRow[3] = t;
+        }
+        // Rational parametrization of semicircle,
+        // see http://jsperf.com/half-circle-parametrization
+        var t2 = t * t;
+        var dt = 0.5 / (1 + t2);
+        var tc = CSNumber.complex((2 * t) * dt + 0.5, (1 - t2) * dt);
+        noMoreRefinements = (last + 0.5 * step <= last);
+        try {
+            stateInIdx = stateOutIdx = mover.stateIdx;
+            parameterPath(mover, t, tc, lastGoodParam, targetParam);
+            if (traceLog) traceLogRow[4] = niceprint(mover.param);
+            opMover.updatePosition(mover);
+            for (i = 0; i < deps.length; ++i) {
+                el = deps[i];
+                op = geoOps[el.type];
+                stateInIdx = stateOutIdx = el.stateIdx;
+                if (op.computeParameters)
+                    op.computeParameters(el);
+                op.updatePosition(el);
+            }
+            last = t; // successfully traced up to t
+            step *= 1.25;
+            t += step;
+            if (t >= 1) t = 1;
+            stateSwapBad(); // may become good if we complete without failing
+        } catch (e) {
+            if (e !== RefineException)
+                throw e;
+            step *= 0.5; // reduce step size
+            t = last + step;
+        }
+    }
+    if (!tracingFailed) {
+        move.lastGoodParam = targetParam;
+        stateContinueFromHere();
+    }
+    tracingStateReport(tracingFailed);
+    for (i = 0; i < deps.length; ++i) {
+        el = deps[i];
+        op = geoOps[el.type];
+        isShowing(el, op);
+    }
+}
+
+function tracingStateReport(failed) {
+    var arg = instanceInvocationArguments.tracingStateReport;
+    if (typeof arg === "string") {
+        document.getElementById(arg).textContent =
+            failed ? "BAD" : "GOOD";
+    }
+}
+
+var traceLog = null;
+var traceLogRow = [];
+
+if (instanceInvocationArguments.enableTraceLog) {
+    traceLog = [traceLogRow];
+    globalInstance.formatTraceLog = formatTraceLog;
+}
+
+function formatTraceLog(save) {
+    var tbody = '<tbody>' + traceLog.map(function(row) {
+        var action = row[row.length - 1];
+        var cls;
+        if (/^Normal/.test(action))
+            cls = "normal";
+        else if (/refine.?$/.test(action))
+            cls = "refine";
+        else
+            cls = "other";
+        if (row[0] !== "")
+            cls = "initial " + cls;
+        else
+            cls = "refined " + cls;
+        return '<tr class="' + cls + '">' + row.map(function(cell) {
+            return '<td>' + cell + '</td>';
+        }).join('') + '</tr>\n';
+    }).join('') + '</tbody>';
+    var cols = [
+        'lastGoodParam', 'targetParam', 'last', 't', 'param',
+        'do1n1', 'do1n2', 'do2n1', 'do2n2', 'do1o2', 'dn1n2', 'cost',
+        'n1', 'n2', 'o1', 'o2', 'case'
+    ];
+    var thead = '<thead>' + cols.map(function(cell) {
+        return '<th>' + cell + '</th>';
+    }).join('') + '</thead>';
+    var table1 = '<table id="t1">' + thead + tbody + '</table>';
+    var css = [
+        'html, body { margin: 0px; padding: 0px; }',
+        'td { white-space: nowrap; border: 1px solid black; }',
+        'table { border-collapse: collapse; margin: 0px; }',
+        'thead th { background: #fff; }',
+        'tr.initial.normal td { background: #0ff; }',
+        'tr.refined.normal td { background: #0f0; }',
+        'tr.initial.refine td { background: #f0f; }',
+        'tr.refined.refine td { background: #ff0; }',
+        'tr.other td { background: #f00; }',
+    ].join('\n');
+    var html = '<html><head><title>Tracing report</title>' +
+        '<style type="text/css">' + css + '</style></head><body>' +
+        table1 + '</body></html>';
+    var type = save ? 'application/octet-stream' : 'text/html';
+    var blob = new Blob([html], {
+        'type': type
+    });
+    var uri = window.URL.createObjectURL(blob);
+    // var uri = 'data:text/html;base64,' + window.btoa(html);
+    return uri;
+}
+
+function getStateComplexNumber() {
+    var i = stateInIdx;
+    stateInIdx += 2;
+    return CSNumber.complex(stateIn[i], stateIn[i + 1]);
+}
+
+function getStateComplexVector(n) {
+    var lst = new Array(n);
+    for (var i = 0; i < n; ++i)
+        lst[i] = getStateComplexNumber();
+    return List.turnIntoCSList(lst);
+}
+
+function putStateComplexNumber(c) {
+    stateOut[stateOutIdx] = c.value.real;
+    stateOut[stateOutIdx + 1] = c.value.imag;
+    stateOutIdx += 2;
+}
+
+function putStateComplexVector(v) {
+    for (var i = 0, n = v.value.length; i < n; ++i)
+        putStateComplexNumber(v.value[i]);
+}
+
 function tracing2(n1, n2) {
     var security = 3;
 
