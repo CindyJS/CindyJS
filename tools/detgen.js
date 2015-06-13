9feb41640755808caa54656823db6f74d7668d80
Martin von Gagern <gagern@ma.tum.de>
2015-06-13 13:17:01 +0200

    Try to make det4m more efficient
    Now we store intermediate body and row array values so we don't have to dig
    through the nested objects for each matrix element.  We also reorder things
    so that we can reuse some variables for the elements, thus reducing the
    total number of variables.

--- tools/detgen.js
+++ tools/detgen.js
@@ -4,12 +4,6 @@ function cplx(r, i) {
     return {r: r, i: i};
 }
 
-function melt(i, j) {
-    // leading + tells JIT compiler that this is always double
-    var elt = "+m.value[" + i + "].value[" + j + "].value";
-    return cplx(elt + ".real", elt + ".imag");
-}
-
 function prodp(a, b) {
     return cplx(a.r + " * " + b.r + " - " + a.i + " * " + b.i,
                 a.r + " * " + b.i + " + " + a.i + " * " + b.r);
@@ -32,27 +26,48 @@ function det2e(a, b, c, d) {
     return sub(prodp(a, d), prodm(b, c));
 }
 
-function cache(n, z) {
-    console.log("    var " + n + "r = " + z.r + ";");
-    console.log("    var " + n + "i = " + z.i + ";");
+function cache(n, z, reuse) {
+    var v = reuse ? "" : "var ";
+    console.log("    " + v + n + "r = " + z.r + ";");
+    console.log("    " + v + n + "i = " + z.i + ";");
     return cplx(n + "r", n + "i");
 }
 
 function gen_det4m() {
     console.log("List.det4m = function(m) {");
     console.log("    // auto-generated code, see detgen.js");
-    var i, j, m = Array(4);
-    for (i = 0; i < 4; ++i) {
+    console.log("    var body = m.value;");
+    console.log("    var row = body[0].value;");
+    console.log("    var elt = row[0].value;");
+    var i, j, m = Array(4), celt = cplx("+elt.real", "+elt.imag");
+    var a = [], b = [], d;
+    for (i = 0; i < 2; ++i) {
         m[i] = Array(4);
-        for (j = 0; j < 4; ++j)
-            m[i][j] = cache("m" + i + j, melt(i, j));
+        if (i > 0)
+            console.log("    row = body[" + i + "].value;");
+        for (j = 0; j < 4; ++j) {
+            if (i + j > 0)
+                console.log("    elt = row[" + j + "].value;");
+            m[i][j] = cache("m" + i + j, celt);
+        }
     }
-    var a = [], b = [];
+    for (i = 0; i < 4; ++i) {
+        for (j = i + 1; j < 4; ++j) {
+            d = det2e(m[0][i], m[0][j], m[1][i], m[1][j]);
+            a.push(cache("a" + i + j, d));
+        }
+    }
+    for (i = 2; i < 4; ++i) {
+        m[i] = Array(4);
+        console.log("    row = body[" + i + "].value;");
+        for (j = 0; j < 4; ++j) {
+            console.log("    elt = row[" + j + "].value;");
+            m[i][j] = cache("m" + (i - 2) + j, celt, true);
+        }
+    }
     for (i = 0; i < 4; ++i) {
         for (j = i + 1; j < 4; ++j) {
             var d;
-            d = det2e(m[0][i], m[0][j], m[1][i], m[1][j]);
-            a.push(cache("a" + i + j, d));
             d = det2e(m[2][i], m[2][j], m[3][i], m[3][j]);
             b.push(cache("b" + i + j, d));
         }
