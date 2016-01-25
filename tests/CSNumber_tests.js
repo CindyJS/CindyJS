09ef9a325c22359de5ffad80e6cc38f3405343b4
Stefan Kranich <stefan.kranich@ma.tum.de>
2016-01-25 17:19:49 +0100

    Make branch cut of CSNumber.sqrt consistent with Cinderella

--- tests/CSNumber_tests.js
+++ tests/CSNumber_tests.js
@@ -189,7 +189,7 @@ describe("Trigonometry", function() {
   });
 
   it("sqrt", function() {
-    var sqrt_f_a = CSNumber.complex(10.000031249755862,-0.024999921875854481);
+    var sqrt_f_a = CSNumber.complex(-10.000031249755862,0.024999921875854481);
     var sqrt_f_b = CSNumber.complex(0.55589297025142115,0.89945371997393364);
 
     expect(CSNumber._helper.isAlmostEqual(sqrt_f_a, CSNumber.sqrt(f_a))).toBe(true);
