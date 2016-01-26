6c689f3706ad671879b9cb19f4d463a5dac38fde
Stefan Kranich <stefan.kranich@ma.tum.de>
2016-01-26 09:12:38 +0100

    Move plugins to plugins directory

--- make/build.js
+++ make/build.js
@@ -238,7 +238,7 @@ module.exports = function build(settings, task) {
         .concat(c3d_primitives.map(
             function(name) { return name + "-frag.glsl"; }));
     var c3d_str_res = c3d_shaders.map(function(name) {
-        return "src/str/cindy3d/" + name;
+        return "plugins/cindy3d/src/str/" + name;
     });
     var c3d_mods = [
         "ShaderProgram",
@@ -275,13 +275,13 @@ module.exports = function build(settings, task) {
             source_map_format: "V3",
             source_map_location_mapping: [
                 "build/js/|",
-                "src/js/|../../src/js/",
+                "src/js/|../../plugins/cindy3d/src/js/",
             ],
-            output_wrapper_file: "src/js/cindy3d/Cindy3D.js.wrapper",
+            output_wrapper_file: "plugins/cindy3d/src/js/Cindy3D.js.wrapper",
             js_output_file: "build/js/Cindy3D.js",
-            externs: "src/js/cindy3d/cindyjs.externs",
+            externs: "plugins/cindy3d/src/js/cindyjs.externs",
             js: ["build/js/c3dres.js"].concat(c3d_mods.map(function(name) {
-                return "src/js/cindy3d/" + name + ".js";
+                return "plugins/cindy3d/src/js/" + name + ".js";
             })),
         };
         if (this.setting("cindy3d-dbg") !== undefined) {
@@ -302,7 +302,7 @@ module.exports = function build(settings, task) {
 
     var cgl_primitives = "sphere cylinder triangle texq".split(" ");
 
-    var cgl_str_res = glob.sync("src/str/cindygl/*.glsl");
+    var cgl_str_res = glob.sync("plugins/cindygl/src/str/*.glsl");
     
     var cgl_mods = [
         "Init",
@@ -339,13 +339,13 @@ module.exports = function build(settings, task) {
             source_map_format: "V3",
             source_map_location_mapping: [
                 "build/js/|",
-                "src/js/|../../src/js/",
+                "src/js/|../../plugins/cindygl/src/js/",
             ],
-            output_wrapper_file: "src/js/cindygl/CindyGL.js.wrapper",
+            output_wrapper_file: "plugins/cindygl/src/js/CindyGL.js.wrapper",
             js_output_file: "build/js/CindyGL.js",
-            externs: "src/js/cindygl/cindyjs.externs",
+            externs: "plugins/cindygl/src/js/cindyjs.externs",
             js: ["build/js/cglres.js"].concat(cgl_mods.map(function(name) {
-                return "src/js/cindygl/" + name + ".js";
+                return "plugins/cindygl/src/js/" + name + ".js";
             })),
         };
         if (this.setting("cindygl-dbg") !== undefined) {
@@ -435,7 +435,7 @@ module.exports = function build(settings, task) {
     });
 
     task("katex-plugin", [], function() {
-        this.copy("src/js/katex/katex-plugin.js", "build/js/katex-plugin.js");
+        this.copy("plugins/katex/src/js/katex-plugin.js", "build/js/katex-plugin.js");
     });
 
     task("katex", ["katex_src", "katex-plugin"]);
