f613b16bc5d51a7506b8c266bd6a7394e3d8f6eb
Martin von Gagern <gagern@ma.tum.de>
2016-02-08 11:42:47 +0100

    Move render steps out of render function
    Since there is no shared state between the different render step functions,
    including them in the main render function only adds confusionn and
    indentation.  Moving them out makes things clearer and easier to reorganize.

--- src/js/libgeo/GeoRender.js
+++ src/js/libgeo/GeoRender.js
@@ -1,172 +1,172 @@
-function render() {
-
-    function drawgeopoint(el) {
-        if (!el.isshowing || el.visible === false || !List._helper.isAlmostReal(el.homog))
-            return;
-        var col = el.color;
-        if (el.behavior) {
-            col = el.color; //TODO Anpassen
-            // col=List.realVector([0,0,1]);
+function drawgeopoint(el) {
+    if (!el.isshowing || el.visible === false || !List._helper.isAlmostReal(el.homog))
+        return;
+    var col = el.color;
+    if (el.behavior) {
+        col = el.color; //TODO Anpassen
+        // col=List.realVector([0,0,1]);
+    }
+    evaluator.draw$1([el.homog], {
+        size: el.size,
+        color: col,
+        alpha: el.alpha
+    });
+    if (el.labeled) {
+        var lbl = el.printname || el.name || "P";
+        var lpos = el.labelpos || {
+            'x': 3,
+            'y': 3
+        };
+        var textsize = el.textsize || 12;
+        var bold = (el.textbold === true);
+        var italics = (el.textitalics === true);
+        var family = el.text_fontfamily || defaultAppearance.fontFamily;
+        var dist = lpos.x * lpos.x + lpos.y * lpos.y;
+        var factor = 1.0;
+        if (dist > 0) {
+            factor = 1.0 + el.size.value.real / Math.sqrt(dist);
         }
-        evaluator.draw$1([el.homog], {
-            size: el.size,
-            color: col,
-            alpha: el.alpha
-        });
-        if (el.labeled) {
-            var lbl = el.printname || el.name || "P";
-            var lpos = el.labelpos || {
-                'x': 3,
-                'y': 3
-            };
-            var textsize = el.textsize || 12;
-            var bold = (el.textbold === true);
-            var italics = (el.textitalics === true);
-            var family = el.text_fontfamily || defaultAppearance.fontFamily;
-            var dist = lpos.x * lpos.x + lpos.y * lpos.y;
-            var factor = 1.0;
-            if (dist > 0) {
-                factor = 1.0 + el.size.value.real / Math.sqrt(dist);
+        evaluator.drawtext$2(
+            [el.homog, General.wrap(lbl)], {
+                'x_offset': General.wrap(factor * lpos.x),
+                'y_offset': General.wrap(factor * lpos.y),
+                'size': General.wrap(textsize),
+                'bold': General.wrap(bold),
+                'italics': General.wrap(italics),
+                'family': General.wrap(family)
+            });
+    }
+}
+
+function drawgeoarc(el) {
+    if (!el.isshowing || el.visible === false)
+        return;
+
+    var modifs = {};
+    modifs.color = el.color;
+    modifs.alpha = el.alpha;
+    modifs.size = el.size;
+
+    // check if we have filled: true
+    var df = el.filled ? "F" : "D";
+
+    eval_helper.drawarc(el, modifs, df);
+}
+
+
+function drawgeoconic(el) {
+    if (!el.isshowing || el.visible === false)
+        return;
+
+    var modifs = {};
+    modifs.color = el.color;
+    modifs.alpha = el.alpha;
+    modifs.size = el.size;
+
+    eval_helper.drawconic(el.matrix, modifs);
+
+
+}
+
+function drawgeoline(el) {
+    var pt1, pt2;
+    if (!el.isshowing || el.visible === false || !List._helper.isAlmostReal(el.homog))
+        return;
+
+    if (el.kind === "S") {
+        // Segments always join their endpoints.
+        evaluator.draw$2(
+            [el.startpos, el.endpos], {
+                overhang: el.overhang,
+                dashtype: el.dashtype,
+                size: el.size,
+                color: el.color,
+                alpha: el.alpha,
+                arrow: el.arrow,
+                arrowsize: el.arrowsize,
+                arrowposition: el.arrowposition,
+                arrowshape: el.arrowshape,
+                arrowsides: el.arrowsides,
+            });
+        return;
+    }
+    if (el.clip.value === "end" && el.type === "Join") {
+        // Lines clipped to their defining points join these.
+        pt1 = csgeo.csnames[el.args[0]];
+        pt2 = csgeo.csnames[el.args[1]];
+        evaluator.draw$2(
+            [pt1.homog, pt2.homog], {
+                overhang: el.overhang,
+                dashtype: el.dashtype,
+                size: el.size,
+                color: el.color,
+                alpha: el.alpha
+            });
+        return;
+    }
+    if (el.clip.value === "inci") {
+        // Figuring out incident points here.
+        var li = [];
+        var xmin = [+1000000, 0];
+        var xmax = [-1000000, 0];
+        var ymin = [+1000000, 0];
+        var ymax = [-1000000, 0];
+        for (var i = 0; i < el.incidences.length; i++) {
+            var pt = csgeo.csnames[el.incidences[i]].homog;
+            var x = pt.value[0];
+            var y = pt.value[1];
+            var z = pt.value[2];
+
+            if (!CSNumber._helper.isAlmostZero(z)) {
+                x = CSNumber.div(x, z);
+                y = CSNumber.div(y, z);
+                if (CSNumber._helper.isAlmostReal(x) && CSNumber._helper.isAlmostReal(y)) {
+                    if (x.value.real < xmin[0]) {
+                        xmin = [x.value.real, pt];
+                    }
+                    if (x.value.real > xmax[0]) {
+                        xmax = [x.value.real, pt];
+                    }
+                    if (y.value.real < ymin[0]) {
+                        ymin = [y.value.real, pt];
+                    }
+                    if (y.value.real > ymax[0]) {
+                        ymax = [y.value.real, pt];
+                    }
+                }
             }
-            evaluator.drawtext$2(
-                [el.homog, General.wrap(lbl)], {
-                    'x_offset': General.wrap(factor * lpos.x),
-                    'y_offset': General.wrap(factor * lpos.y),
-                    'size': General.wrap(textsize),
-                    'bold': General.wrap(bold),
-                    'italics': General.wrap(italics),
-                    'family': General.wrap(family)
-                });
         }
-    }
+        if ((xmax[0] - xmin[0]) > (ymax[0] - ymin[0])) {
+            pt1 = xmin[1];
+            pt2 = xmax[1];
+        } else {
+            pt1 = ymin[1];
+            pt2 = ymax[1];
 
-    function drawgeoarc(el) {
-        if (!el.isshowing || el.visible === false)
-            return;
-
-        var modifs = {};
-        modifs.color = el.color;
-        modifs.alpha = el.alpha;
-        modifs.size = el.size;
-
-        // check if we have filled: true
-        var df = el.filled ? "F" : "D";
-
-        eval_helper.drawarc(el, modifs, df);
-    }
-
-
-    function drawgeoconic(el) {
-        if (!el.isshowing || el.visible === false)
-            return;
-
-        var modifs = {};
-        modifs.color = el.color;
-        modifs.alpha = el.alpha;
-        modifs.size = el.size;
-
-        eval_helper.drawconic(el.matrix, modifs);
-
-
-    }
-
-    function drawgeoline(el) {
-        var pt1, pt2;
-        if (!el.isshowing || el.visible === false || !List._helper.isAlmostReal(el.homog))
-            return;
-
-        if (el.kind === "S") {
-            // Segments always join their endpoints.
+        }
+        if (pt1 !== pt2) {
             evaluator.draw$2(
-                [el.startpos, el.endpos], {
-                    overhang: el.overhang,
+                [pt1, pt2], {
                     dashtype: el.dashtype,
                     size: el.size,
                     color: el.color,
                     alpha: el.alpha,
-                    arrow: el.arrow,
-                    arrowsize: el.arrowsize,
-                    arrowposition: el.arrowposition,
-                    arrowshape: el.arrowshape,
-                    arrowsides: el.arrowsides,
+                    overhang: el.overhang
                 });
             return;
         }
-        if (el.clip.value === "end" && el.type === "Join") {
-            // Lines clipped to their defining points join these.
-            pt1 = csgeo.csnames[el.args[0]];
-            pt2 = csgeo.csnames[el.args[1]];
-            evaluator.draw$2(
-                [pt1.homog, pt2.homog], {
-                    overhang: el.overhang,
-                    dashtype: el.dashtype,
-                    size: el.size,
-                    color: el.color,
-                    alpha: el.alpha
-                });
-            return;
-        }
-        if (el.clip.value === "inci") {
-            // Figuring out incident points here.
-            var li = [];
-            var xmin = [+1000000, 0];
-            var xmax = [-1000000, 0];
-            var ymin = [+1000000, 0];
-            var ymax = [-1000000, 0];
-            for (var i = 0; i < el.incidences.length; i++) {
-                var pt = csgeo.csnames[el.incidences[i]].homog;
-                var x = pt.value[0];
-                var y = pt.value[1];
-                var z = pt.value[2];
-
-                if (!CSNumber._helper.isAlmostZero(z)) {
-                    x = CSNumber.div(x, z);
-                    y = CSNumber.div(y, z);
-                    if (CSNumber._helper.isAlmostReal(x) && CSNumber._helper.isAlmostReal(y)) {
-                        if (x.value.real < xmin[0]) {
-                            xmin = [x.value.real, pt];
-                        }
-                        if (x.value.real > xmax[0]) {
-                            xmax = [x.value.real, pt];
-                        }
-                        if (y.value.real < ymin[0]) {
-                            ymin = [y.value.real, pt];
-                        }
-                        if (y.value.real > ymax[0]) {
-                            ymax = [y.value.real, pt];
-                        }
-                    }
-                }
-            }
-            if ((xmax[0] - xmin[0]) > (ymax[0] - ymin[0])) {
-                pt1 = xmin[1];
-                pt2 = xmax[1];
-            } else {
-                pt1 = ymin[1];
-                pt2 = ymax[1];
-
-            }
-            if (pt1 !== pt2) {
-                evaluator.draw$2(
-                    [pt1, pt2], {
-                        dashtype: el.dashtype,
-                        size: el.size,
-                        color: el.color,
-                        alpha: el.alpha,
-                        overhang: el.overhang
-                    });
-                return;
-            }
-            // otherwise fall through
-        }
-        // Default: draw an unclipped line
-        evaluator.draw$1([el.homog], {
-            dashtype: el.dashtype,
-            size: el.size,
-            color: el.color,
-            alpha: el.alpha
-        });
+        // otherwise fall through
     }
+    // Default: draw an unclipped line
+    evaluator.draw$1([el.homog], {
+        dashtype: el.dashtype,
+        size: el.size,
+        color: el.color,
+        alpha: el.alpha
+    });
+}
+
+function render() {
 
     var i;
 
