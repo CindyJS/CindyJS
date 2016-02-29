318285ad34e72721b7a35a6f790d9a64e18ee516
Martin von Gagern <gagern@ma.tum.de>
2016-02-29 09:25:39 +0100

    Change namespace from constructor-based object to module
    We only ever have one namespace per instance.

--- src/js/libcs/Namespace.js
+++ src/js/libcs/Namespace.js
@@ -52,134 +52,125 @@ var bmpLetters = (function(d, s) {
 var reVarName = new RegExp(
     "^(?:#[1-9]?|[0-9'" + bmpLetters + "]+)$");
 
-/** @constructor */
-function Namespace() {
-    var vars = {
+var namespace = {};
+
+// Initialize preset variables
+namespace.vars = (function() {
+    var preset = {
         pi: CSNumber.real(Math.PI),
         i: CSNumber.complex(0, 1),
         'true': General.bool(true),
         'false': General.bool(false),
         '#': nada,
     };
-    this.vars = [];
-    for (var name in vars)
-        this.vars[name] = {
+    var vars = [];
+    for (var name in preset)
+        vars[name] = {
             ctype: 'variable',
             name: name,
-            stack: [vars[name]]
+            stack: [preset[name]]
         };
+    return vars;
+})();
 
-    this.isVariable = function(a) {
-        return this.vars.hasOwnProperty(a);
+namespace.isVariable = function(a) {
+    return this.vars.hasOwnProperty(a);
+};
+
+namespace.isVariableName = function(a) {
+    return reVarName.test(a);
+};
+
+namespace.create = function(code) {
+    var v = {
+        'ctype': 'variable',
+        'stack': [null],
+        'name': code
     };
+    this.vars[code] = v;
+    return v;
+};
 
-    this.isVariableName = function(a) {
-        return reVarName.test(a);
-    };
+namespace.newvar = function(code) {
+    var v = this.vars[code];
+    v.stack.push(nada); // nada not null for deeper levels
+    return v;
+};
 
-    this.create = function(code) {
-        var v = {
-            'ctype': 'variable',
-            'stack': [null],
-            'name': code
-        };
-        this.vars[code] = v;
-        return v;
-    };
-
-    this.newvar = function(code) {
-        var v = this.vars[code];
-        v.stack.push(nada); // nada not null for deeper levels
-        return v;
-    };
-
-    this.removevar = function(code) {
-        var stack = this.vars[code].stack;
-        if (stack.length === 0) console.error("Removing non-existing " + code);
-        stack.pop();
-        if (stack.length === 0) console.warn("Removing last " + code);
-    };
+namespace.removevar = function(code) {
+    var stack = this.vars[code].stack;
+    if (stack.length === 0) console.error("Removing non-existing " + code);
+    stack.pop();
+    if (stack.length === 0) console.warn("Removing last " + code);
+};
 
 
-    this.setvar = function(code, val) {
-        var stack = this.vars[code].stack;
-        if (stack.length === 0) console.error("Setting non-existing variable " + code);
-        if (val === undefined) {
-            console.error("Setting variable " + code + " to undefined value");
-            val = nada;
-        }
-        if (val.ctype === 'undefined') {
-            stack[stack.length - 1] = val;
-            return;
-        }
-        var erg = val;
-        if (erg === null) erg = nada; // explicit setting does lift unset state
-        stack[stack.length - 1] = erg;
-    };
+namespace.setvar = function(code, val) {
+    var stack = this.vars[code].stack;
+    if (stack.length === 0) console.error("Setting non-existing variable " + code);
+    if (val === undefined) {
+        console.error("Setting variable " + code + " to undefined value");
+        val = nada;
+    }
+    if (val.ctype === 'undefined') {
+        stack[stack.length - 1] = val;
+        return;
+    }
+    var erg = val;
+    if (erg === null) erg = nada; // explicit setting does lift unset state
+    stack[stack.length - 1] = erg;
+};
 
-    /* // Apparently unused
-    this.setvarnocopy= function(code,val) {
-        var stack=this.vars[code].stack;
-        if (stack.length === 0) console.error("Setting non-existing variable " + code);
-        stack[stack.length-1]=val;
-    };
-    */
+namespace.undefinedWarning = {};
 
-    this.undefinedWarning = {};
+namespace.getvar = function(code) {
 
-    this.getvar = function(code) {
-
-        var stack = this.vars[code].stack;
-        if (stack.length === 0) console.error("Getting non-existing variable " + code);
-        var erg = stack[stack.length - 1];
-        if (erg === null) {
-            if (csgeo.csnames.hasOwnProperty(code)) {
-                return {
-                    'ctype': 'geo',
-                    'value': csgeo.csnames[code]
-                };
-            } else {
-                if (console && console.log && this.undefinedWarning[code] === undefined) {
-                    this.undefinedWarning[code] = true;
-                    console.log("Warning: Accessing undefined variable: " + code);
-                }
+    var stack = this.vars[code].stack;
+    if (stack.length === 0) console.error("Getting non-existing variable " + code);
+    var erg = stack[stack.length - 1];
+    if (erg === null) {
+        if (csgeo.csnames.hasOwnProperty(code)) {
+            return {
+                'ctype': 'geo',
+                'value': csgeo.csnames[code]
+            };
+        } else {
+            if (console && console.log && this.undefinedWarning[code] === undefined) {
+                this.undefinedWarning[code] = true;
+                console.log("Warning: Accessing undefined variable: " + code);
             }
-            return nada;
         }
-        return erg;
-    };
+        return nada;
+    }
+    return erg;
+};
 
-    this.dump = function(code) {
-        var stack = this.vars[code].stack;
-        console.log("*** Dump " + code);
+namespace.dump = function(code) {
+    var stack = this.vars[code].stack;
+    console.log("*** Dump " + code);
 
-        for (var i = 0; i < stack.length; i++) {
-            console.log(i + ":> " + niceprint(stack[i]));
-        }
-    };
+    for (var i = 0; i < stack.length; i++) {
+        console.log(i + ":> " + niceprint(stack[i]));
+    }
+};
 
-    this.vstack = [];
+namespace.vstack = [];
 
-    this.pushVstack = function(v) {
-        this.vstack.push(v);
+namespace.pushVstack = function(v) {
+    this.vstack.push(v);
 
-    };
-    this.popVstack = function() {
-        this.vstack.pop();
-    };
+};
+namespace.popVstack = function() {
+    this.vstack.pop();
+};
 
-    this.cleanVstack = function() {
-        var st = this.vstack;
-        while (st.length > 0 && st[st.length - 1] !== "*") {
-            this.removevar(st[st.length - 1]);
-            st.pop();
-        }
-        if (st.length > 0) {
-            st.pop();
-        }
-    };
-
-
-}
-
-var namespace = new Namespace();
+namespace.cleanVstack = function() {
+    var st = this.vstack;
+    while (st.length > 0 && st[st.length - 1] !== "*") {
+        this.removevar(st[st.length - 1]);
+        st.pop();
+    }
+    if (st.length > 0) {
+        st.pop();
+    }
+};
