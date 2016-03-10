b5454962eba4b8e6f1d9bac87e92eaeafb72cc97
Michael Strobel <strobel@ma.tum.de>
2016-03-10 14:24:00 +0100

    point on line

--- src/js/libgeo/Prover.js
+++ src/js/libgeo/Prover.js
@@ -102,7 +102,7 @@ function checkConjectures() {
             moves = geoOps[el.type].getRandomMove(el);
             // moves are arrays which can have different type: homog, radius etc ...
             moves.forEach(function(newpos) {
-                //if(el.type === "PointOnCircle") debugger;
+                //if(el.type === "PointOnLine") 
                 movepointscr(el, newpos.value, newpos.type);
             });
             // check if conjecture still holds
