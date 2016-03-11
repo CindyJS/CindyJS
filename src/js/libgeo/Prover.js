f84f500be6be4baa1a1a053c7f2b711071cad37e
Michael Strobel <strobel@ma.tum.de>
2016-03-11 12:49:40 +0100

    disable debug

--- src/js/libgeo/Prover.js
+++ src/js/libgeo/Prover.js
@@ -74,7 +74,7 @@ function incidentPC(p, c) {
 }
 
 function checkConjectures() {
-    var debug = true;
+    var debug = false;
     if (debug) console.log("conjectures", conjectures.length);
     //if (!debug) if (conjectures.length === 0) return;
     backupGeo();
