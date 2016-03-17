ee99903faf3592634cd56d1ed4e4e95661bc6ff7
Michael Strobel <strobel@ma.tum.de>
2016-03-17 17:04:02 +0100

    imagesize

--- src/js/libcs/OpImageDrawing.js
+++ src/js/libcs/OpImageDrawing.js
@@ -10,12 +10,19 @@ eval_helper.extractReferenceX = function(w, pos) {
 
 evaluator.imagesize$1 = function(args, modifs) {
     var img = evaluateAndVal(args[0]);
+
     if (img.ctype === 'string') {
         if (images[img.value]) {
             var w = images[img.value].width;
             var h = images[img.value].height;
             return List.realVector([w, h]);
-
+        }
+    }
+    if (img.ctype === 'image') {
+        if (img.value) {
+            var w = img.value.width;
+            var h = img.value.height;
+            return List.realVector([w, h]);
         }
     }
 
