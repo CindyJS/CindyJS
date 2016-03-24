eb648f4b06dc2ce84e028ba139878439e2933322
Martin von Gagern <gagern@ma.tum.de>
2016-03-24 09:13:13 +0100

    Document unicode regexp compression algorithm

--- tools/unicodeCat.js
+++ tools/unicodeCat.js
@@ -1,3 +1,41 @@
+/*
+ * This file creates the code for the unicodeLetters regexp fragment
+ * used in the tokenizer in Parser.js.  It's not run during normal
+ * builds, but instead its output was copied manually.  This script
+ * might be re-run if something about that representation or the
+ * unicode version in use changes.
+ *
+ * The general approach is as follows:
+ * 1. The unicode 8.0.0 category L (Letter) codepoint list is
+ *    downloaded from GitHub and evaluated.  Other categories or
+ *    unicode versions might be added if the need arises.
+ * 2. The list is destructed into ranges of consecutive codepoints.
+ * 3. Now the UTF-16 representation is taken into account. Ranges
+ *    spanning multiple high surrogates are split according to the
+ *    high surrogate. Resulting ranges for which *any* low surrogate
+ *    are permissible are treated specially, since they are combined
+ *    into a single […] for high surrogates, followed by
+ *    [\uDC00-\uDFFF] to match any low surrogate.
+ * 4. The absolute code points are turned into deltas, both between
+ *    the first and the last codepoint of a range and between the last
+ *    codepoint of one range and the first codepoint of the next
+ *    range.  A new high surrogate resets the the reference counter to
+ *    the first codepoint associated with that high surrogate.
+ * 5. A histogram identifies often-used delta values. These will be
+ *    encoded using single characters of a string literal. Less often
+ *    used values will use hexadecimal or even unicode escape
+ *    sequences.  The escape sequences for \\ and \" fall somewhere
+ *    between single characters and hexadecimal escapes.
+ * 6. The output consists of three parts.  The first is an array of
+ *    numbers.  It represents the dictionary used to turn characters
+ *    back into deltas.  Next comes the string literal representing
+ *    all the deltas, and finally the high surrogate ranges for which
+ *    arbitrary low surrogates are permissible.
+ *
+ * The code to decompress this is in Parser.js, and in Parser_test.js
+ * there is a verbatim version of the resulting uncompressed regexp.
+ */
+
 'use strict';
 
 const request = require('request');
