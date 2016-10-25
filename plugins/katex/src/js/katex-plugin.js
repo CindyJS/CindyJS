CindyJS/CindyJS

commit 8be4849eb678d3a8e8db6ce2b7bb78cee7ddb29a

Support newlines in text
This adds support for newlines in text, for drawtext calls, Text elements
and Button elements, both with and without the KaTeX plugin.

There are a number of unresolved issues yet:

1. The line height factor was measuered from Cinderella, but might be
   font-dependent, in which case we might want to measure some specific
   text in an attempt to approximate that value in a better way.

2. Buttons are aligned by their last row of text, where Cinderella
   aligns them by their first row.

3. The actual height of a TeX formula isn't taken into account when
   advancing to the next row, making the combination of KaTeX and multi-line
   text unsuitable for formulas much higher than the regular run of text.

All of these are mentioned in comments inside this code.

plugins/katex/src/js/katex-plugin.js
@@ -148,22 +148,21 @@
 
     var firstMessage = true;
 
-    function katexRenderer(ctx, text, x, y, align) {
-        var fontSize = /(?:^| )([0-9]+)px(?:$| )/.exec(ctx.font);
-        fontSize = fontSize ? +fontSize[1] : 16;
-        var key = fontSize + ":" + text;
+    function katexRenderer(ctx, text, x, y, align, fontSize, lineHeight) {
+        var key = fontSize + "," + lineHeight + ":" + text;
         var fontsMissing = false;
         var fontsToLoad = {};
-        var parts, n, i;
+        var parts, rows, row, n, i, j;
         if (this.cache.hasOwnProperty(key)) {
-            parts = this.cache[key];
-            n = parts.length;
+            rows = this.cache[key];
         } else {
             var opts = {
                 fontSize: fontSize,
                 macros: macros
             };
             parts = text.split("$");
+            row = [];
+            rows = [row];
             n = parts.length;
             if (n > 1 && !allScriptsLoaded()) {
                 if (firstMessage) {
@@ -173,23 +172,35 @@
                 haveToWait(this.instance);
                 return;
             }
-            for (i = 0; i < n; i += 2) {
-                parts[i] = new textBox(ctx, parts[i]);
-            }
-            for (i = 1; i < n; i += 2) {
-                try {
-                    parts[i] = katex.canvasBox(parts[i], ctx, opts);
-                    for (var font in parts[i].fontsUsed) {
-                        var fontState = fonts[font];
-                        if (fontState !== true) {
-                            fontsMissing = true;
-                            if (fontState === undefined)
-                                fontsToLoad[font] = true;
+            for (i = 0; i < n; ++i) {
+                var part = parts[i];
+                var box;
+                if ((i & 1) === 0) { // plain text not TeX
+                    if (part.indexOf("\n") === -1) {
+                        row.push(new textBox(ctx, part));
+                    } else {
+                        var rows2 = part.split("\n");
+                        row.push(new textBox(ctx, rows2[0]));
+                        for (j = 1; j < rows2.length; ++j) {
+                            row = [new textBox(ctx, rows2[j])];
+                            rows.push(row);
                         }
                     }
-                } catch(e) {
-                    console.error(e);
-                    parts[i] = new textBox(ctx, "$" + parts[i] + "$");
+                } else {
+                    try {
+                        row.push(katex.canvasBox(parts[i], ctx, opts));
+                        for (var font in parts[i].fontsUsed) {
+                            var fontState = fonts[font];
+                            if (fontState !== true) {
+                                fontsMissing = true;
+                                if (fontState === undefined)
+                                    fontsToLoad[font] = true;
+                            }
+                        }
+                    } catch(e) {
+                        console.error(e);
+                        row.push(new textBox(ctx, "$" + parts[i] + "$"));
+                    }
                 }
             }
             if (++this.misses === 1024) {
@@ -197,7 +208,7 @@
                 this.cache = {};
             }
             if (!fontsMissing) {
-                this.cache[key] = parts;
+                this.cache[key] = rows;
             }
         }
         if (fontsMissing) {
@@ -207,13 +218,20 @@
             }
             haveToWait(this.instance);
         } else {
-            var total = 0;
-            for (i = 0; i < n; ++i)
-                total += parts[i].width;
-            x -= align * total;
-            for (i = 0; i < n; ++i) {
-                parts[i].renderAt(x, y);
-                x += parts[i].width;
+            for (i = 0; i < rows.length; ++i) {
+                var total = 0;
+                var pos = x;
+                row = rows[i];
+                n = row.length;
+                for (j = 0; j < n; ++j)
+                    total += row[j].width;
+                var pos = x - align * total;
+                for (j = 0; j < n; ++j) {
+                    row[j].renderAt(pos, y);
+                    pos += row[j].width;
+                }
+                y += lineHeight;
+                // TODO: take vertical dimensions of formulas into account
             }
         }
     };
@@ -235,10 +253,23 @@
         while (element.firstChild)
             element.removeChild(element.firstChild);
         for (var i = 0; i < n; ++i) {
+            var text = parts[i];
             if ((i & 1) === 0) {
-                element.appendChild(document.createTextNode(parts[i]));
+                if (text.indexOf("\n") !== -1) {
+                    var rows = text.split("\n");
+                    console.log(rows);
+                    element.appendChild(document.createTextNode(rows[0]));
+                    for (var j = 1; j < rows.length; ++j) {
+                        element.appendChild(document.createElement("br"));
+                        element.appendChild(document.createTextNode(rows[j]));
+                    }
+                } else {
+                    element.appendChild(document.createTextNode(text));
+                }
             } else {
-                katex.render(parts[i], element, opts);
+                var span = document.createElement("span");
+                katex.render(text, span, opts);
+                element.appendChild(span);
             }
         }
     }

examples/148_mltext.html
@@ -1,0 +1,43 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+<title>Multi-line text</title>
+<meta charset="UTF-8">
+<link rel="stylesheet" href="../build/js/CindyJS.css">
+<script type="text/javascript" src="../build/js/Cindy.js"></script>
+<script type="text/javascript" src="../build/js/katex-plugin.js"></script>
+<script id="csdraw" type="text/x-cindyscript">
+drawtext((0, 0), "A1
+A2
+A3
+A4
+A5", size->20);
+</script>
+<script type="text/javascript">
+
+for(var i = 1; i <= 2; ++i)
+CindyJS({
+  ports: [{id: "CSCanvas" + i, width: 500, height: 500, axes:true}],
+  scripts: "cs*",
+  language: "en",
+  defaultAppearance: {},
+  use: i === 2 ? ["katex"] : [],
+  geometry: [
+    {name:"A", type:"Free", pos:[0,0], size:1},
+    {name:"B", type:"Free", pos:[0,-4.6], size:1},
+    {name:"T0", type:"Text", pos:[2,0], size:20, text:"B1\nB2\nB3\nB4\nB5"},
+    {name:"T1", type:"Text", pos:[4,0], size:20, text:"C1\n$C_2$\nC3\n$C\n_4$\nC5"},
+    {name:"B0", type:"Button", pos:[-5,0], size:20, text:"D1\n$D_2$\nD3"}
+  ]
+});
+
+</script>
+</head>
+
+<body style="font-family:Arial;">
+  <div id="CSCanvas1" style="border:2px solid black"></div>
+  <div id="CSCanvas2" style="border:2px solid black"></div>
+</body>
+
+</html>

src/js/libcs/OpDrawing.js
@@ -876,14 +876,34 @@ eval_helper.drawpolygon = function(args, modifs, df, cycle) {
 
 };
 
-// This is a hook: the following function may get replaced by a plugin.
-var textRendererCanvas = function(ctx, text, x, y, align) {
+function defaultTextRendererCanvas(ctx, text, x, y, align, size, lineHeight) {
+    if (text.indexOf("\n") !== -1) {
+        text.split("\n").forEach(function(row) {
+            defaultTextRendererCanvas(ctx, row, x, y, align, size);
+            y += lineHeight;
+        });
+        return;
+    }
     var width = ctx.measureText(text).width;
     ctx.fillText(text, x - width * align, y);
-};
+}
+
+// This is a hook: the following function may get replaced by a plugin.
+var textRendererCanvas = defaultTextRendererCanvas;
 
 // This is a hook: the following function may get replaced by a plugin.
 var textRendererHtml = function(element, text, font) {
+    if (text.indexOf("\n") !== -1) {
+        // TODO: find a way to align the element by its FIRST row
+        // as Cinderella does it, instead of by the last row as we do now.
+        var rows = text.split("\n");
+        element.textContent = rows[0];
+        for (var i = 1; i < rows.length; ++i) {
+            element.appendChild(document.createElement("br"));
+            element.appendChild(document.createTextNode(rows[i]));
+        }
+        return;
+    }
     element.textContent = text;
 };
 
@@ -917,9 +937,17 @@ evaluator.drawtext$2 = function(args, modifs, callback) {
         Render2D.family);
     csctx.font = font;
     if (callback) {
-        callback(txt, font, xx, yy, Render2D.align);
+        callback(txt, font, xx, yy, Render2D.align, size);
     } else {
-        textRendererCanvas(csctx, txt, xx, yy, Render2D.align);
+        textRendererCanvas(
+            csctx, txt, xx, yy, Render2D.align, size, size * 1.45);
+        /* The value of 1.45 for the line height agrees reasonably well
+         * with the default font of Cinderella on OS X,
+         * but it might well be that the Java line height
+         * is read from the font file,
+         * so that other fonts should use other line heights.
+         * Not sure whether we can reasonably reproduce this.
+         */
     }
 
     return nada;

commit 3ddb1cb1b852e1e23e23aba720c45fa4e956ae3e

Correctly obtain used fonts from KaTeX box
This fixes a bug introduced by 8be4849eb678d3a8e8db6ce2b7bb78cee7ddb29a
where we no longer detect which fonts are actually being used by a given
formula, thus leading to incorrect rendering while the fonts load, and even
incorrect rendering afterwards as results based on wrong metrics get cached.

plugins/katex/src/js/katex-plugin.js
@@ -188,8 +188,9 @@
                     }
                 } else {
                     try {
-                        row.push(katex.canvasBox(parts[i], ctx, opts));
-                        for (var font in parts[i].fontsUsed) {
+                        box = katex.canvasBox(parts[i], ctx, opts);
+                        row.push(box);
+                        for (var font in box.fontsUsed) {
                             var fontState = fonts[font];
                             if (fontState !== true) {
                                 fontsMissing = true;