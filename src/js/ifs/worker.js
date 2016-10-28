de6cfdd0c0aa1538fabf63865cd23856ec59639c
Martin von Gagern <gagern@ma.tum.de>
2016-10-28 15:17:43 +0200

    Work around Firefox memory leak for ImageBitmaps

--- src/js/ifs/worker.js
+++ src/js/ifs/worker.js
@@ -6,8 +6,10 @@
 var nextInit = null;
 var asm = null;
 var buffer = null;
+var imgSize = 0;
 var imgPtr = null;
 var imgData = null;
+var imgTransfer = null;
 var width = 0;
 var height = 0;
 var Module = {};
@@ -52,7 +54,7 @@ function init(d) {
     height = d.height;
     var n = d.trafos.length;
     var fixedSize = 96;
-    var imgSize = width * height * 4;
+    imgSize = width * height * 4;
     var trafoSize = (112 + 4) * n;
     if (trafoSize % 8) trafoSize += 8 - (trafoSize % 8);
     var minSize = fixedSize + trafoSize + imgSize;
@@ -96,17 +98,42 @@ function init(d) {
 
 function next(d) {
     if (d.buffer) {
-        buffer = d.buffer;
-        link();
-        imgData = null; // won't be needing this
+        if (imgTransfer) {
+            imgTransfer = d.buffer;
+        } else {
+            buffer = d.buffer;
+            link();
+            imgData = null; // won't be needing this
+        }
     }
     if (nextInit) {
         init(nextInit);
         nextInit = null;
     }
+
     asm._real(Math.pow(10, age) | 0, 0);
     if (age < 5) ++age;
-    if (typeof createImageBitmap === "function") {
+
+    var ff = /Firefox\/(\d+)\.(\d+)/.exec(navigator.userAgent);
+    if (ff) {
+        // current versions of Firefox have serious problems with ImageBitmaps:
+        // bug 1271504: will pre-multiply ImageBitmap repeatedly
+        // bug 1312148: massive memory leak creating ImageBitmap instances
+        // Furthermore bug 1312139 prevents us from using the fallback
+        // since we cannot transfer a buffer which has been used by asm.js.
+        // So we use a separate buffer (and one more copy step) for transfer.
+        if (!imgTransfer || imgTransfer.byteLength < imgSize)
+            imgTransfer = new ArrayBuffer(imgSize);
+        var t = new Uint8ClampedArray(imgTransfer);
+        t.set(imgData.data);
+        postMessage({
+            generation: generation,
+            buffer: imgTransfer,
+            imgPtr: 0,
+            width: width,
+            height: height
+        }, [imgTransfer]);
+    } else if (typeof createImageBitmap === "function") {
         createImageBitmap(imgData).then(function(bmp) {
             postMessage({
                 generation: generation,
