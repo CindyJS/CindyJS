6c7b1bc0d494cf7f1804d92de95ef623113ff6a5
Aaron Montag <montag@ma.tum.de>
2018-06-04 13:53:30 +0200

    add missing return statement

--- plugins/cindygl/src/str/random.glsl
+++ plugins/cindygl/src/str/random.glsl
@@ -5,4 +5,5 @@ float random() {
   float a = fract(132422.21*sin(dot(plain_pixel, 343433.671228*vec2(.176574+last_rnd, .1131+rnd_))));
   float b = fract(last_rnd*2321.2312*sin(dot(plain_pixel+vec2(rnd_,last_rnd), plain_pixel) * 43758.5453));
   last_rnd = fract(rnd_ + last_rnd + a + b);
+  return last_rnd;
 }
