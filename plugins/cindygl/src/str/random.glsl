4594d9a5e59bde45b0fbd5d33e56a6a4b6649c5a
Aaron Montag <amontag@ma.tum.de>
2018-06-04 11:07:06 +0200

    Revert "CindyGL: enhance random()"

--- plugins/cindygl/src/str/random.glsl
+++ plugins/cindygl/src/str/random.glsl
@@ -2,6 +2,10 @@ uniform float rnd_;
 
 float last_rnd = .1231;
 float random() {
-  last_rnd = fract(rnd_+fract(132422.21*sin(dot(plain_pixel, 343433.671228*vec2(.176574+last_rnd, .1131+rnd_)))));
+  float a = fract(sin(dot(plain_pixel ,vec2(12.9898,78.233))) * 43758.5453);
+  float b = mod(last_rnd*plain_pixel.y*54.21,1.32);
+  last_rnd = mod(a+b+a+sin(b)+b*sin(dot(plain_pixel, vec2(232.121+rnd_,2232.11))+last_rnd*121.11)*129.12+rnd_,1.);
   return last_rnd;
+  
+  //return mod(a+b+rnd_, 1.);
 }
