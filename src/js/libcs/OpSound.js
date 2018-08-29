dfb38cc11fe72c2965fb5b7e5187da9bb8cba93d
pdywilson <pdywilson@gmail.com>
2018-08-29 17:15:59 +0200

    playsin, playfunction

--- src/js/libcs/OpSound.js
+++ src/js/libcs/OpSound.js
@@ -2,24 +2,38 @@
 // and here are the definitions of the sound operators
 //*******************************************************
 
-var sound = {};
-sound.lines = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
-
 evaluator.playsin$1 = function(args, modifs) {
 
     function handleModifs() {
         var erg;
-        if (modifs.line !== undefined) {
-
-            erg = evaluate(modifs.line);
+        if (modifs.amp !== undefined) {
+            erg = evaluate(modifs.amp);
             if (erg.ctype === 'number') {
-                linenumber = Math.floor(erg.value.real);
-                if (linenumber < 0) {
-                    linenumber = 0;
-                }
-                if (linenumber > 10) {
-                    linenumber = 10;
-                }
+                amp = erg.value.real;
+            }
+        }
+        if (modifs.damp !== undefined) {
+            erg = evaluate(modifs.damp);
+            if (erg.ctype === 'number') {
+                damp = erg.value.real;
+            }
+        }
+        if (modifs.duration !== undefined) {
+            erg = evaluate(modifs.duration);
+            if (erg.ctype === 'number') {
+                duration = erg.value.real;
+            }
+        }
+        if (modifs.stop !== undefined) {
+            erg = evaluate(modifs.stop);
+            if (erg.ctype === 'number') {
+                duration = erg.value.real;
+            }
+        }
+        if (modifs.harmonics !== undefined) {
+            erg = evaluate(modifs.harmonics);
+            if (erg.ctype === 'list') {
+                harmonics = erg.value.real;
             }
         }
     }
@@ -45,37 +59,87 @@ evaluator.playsin$1 = function(args, modifs) {
     var context = new AudioContext();
 
     var v0 = evaluateAndVal(args[0]);
-    var linenumber = 0;
-    
+    var freq = v0.value.real;
+    var amp = 1;
+    var damp = 1;
+    var duration = 1;
+
+
     handleModifs();
-    var lines = sound.lines;
-    var f = v0.value.real;
-    
 
-    var arr = [], volume = 0.2, seconds = 0.5, tone = f
 
-    for (var i = 0; i < context.sampleRate * seconds; i++) {
-        arr[i] = sineWaveAt(i, tone) * volume
+    var arr = [];
+
+    for (var i = 0; i < context.sampleRate * duration; i++) {
+        arr[i] = sineWaveAt(i, freq) * amp;
     }
 
     playSound(arr)
 
-    //if (v0.ctype === 'number') {
+    return nada;
 
-        /*
-        if (lines[linenumber] === 0) {
-            // Was bitte sollte die Funktion T an dieser Stelle sein?
-            // lines[linenumber]=T("sin", {freq:f,mul:0.6}).play();
+};
 
+evaluator.playfunction$1 = function(args, modifs) {
 
-        } else {
-            lines[linenumber].set({
-                freq: f
-            });
+    function handleModifs() {
+        var erg;
+        if (modifs.amp !== undefined) {
+            erg = evaluate(modifs.amp);
+            if (erg.ctype === 'number') {
+                amp = erg.value.real;
+            }
         }
-        */
+    }
 
-    //}
+    function playSound(arr) {
+            var buf = new Float32Array(arr.length)
+            for (var i = 0; i < arr.length; i++) buf[i] = arr[i]
+            var buffer = context.createBuffer(1, buf.length, context.sampleRate)
+            buffer.copyToChannel(buf, 0)
+            var source = context.createBufferSource();
+            source.buffer = buffer;
+            source.connect(context.destination);
+            source.start(0);
+        }
+
+
+
+    var context = new AudioContext();
+
+    /*function searchVar(tree,name) {
+      while(true){
+        for(var i = 0; i < 2; i++) {
+          if tree.args[0].name == name){
+            return
+          }
+        }
+      }
+    }*/
+
+    var v0 = args[0];
+    //var runv = args[0].args[0].args[0].args[0].args[1].name;
+    var runv = "x";
+    namespace.newvar(runv);
+
+    var amp = 1;
+    var length = 1;
+    var freq = 330;
+    handleModifs();
+
+    var arr = [], amp = 0.2, seconds = 1
+
+
+
+    for (var i = 0; i < context.sampleRate * seconds; i++) {
+        namespace.setvar(runv, CSNumber.real(i/context.sampleRate));
+        var erg = evaluate(v0);
+        arr[i] = erg.value.real * amp;
+
+    }
+
+    playSound(arr)
+    namespace.removevar(runv);
     return nada;
 
 };
