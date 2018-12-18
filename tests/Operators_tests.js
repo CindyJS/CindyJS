b3d062a699fc09abe390dbd57d20c5c8e6fd3fd3
Michael Strobel <mstrobel@biodataanalysis.de>
2018-12-18 12:04:59 +0100

    more tests

--- tests/Operators_tests.js
+++ tests/Operators_tests.js
@@ -7,8 +7,6 @@ var CindyJS = require("../build/js/Cindy.plain.js");
 var cdy = CindyJS({
             isNode: true,
             csconsole: null,
-            geometry: [
-            ],
         });
 
 function itCmd(command, expected) {
@@ -20,11 +18,15 @@ function itCmd(command, expected) {
 describe("Operators: format", function(){
     itCmd('format(1.23456, 0)', '1');
     itCmd('format(1.23456, 1)', '1.2');
-
+    itCmd('format(1234567.89, 1)', '1234567.9');
     itCmd('format(1.23456, -1)', '1');
 
+    itCmd('format(123456.000, 2)', '123456');
+
     // modifiers
-    itCmd('format(1.23456, 2, delimiter->",")', '1,23');
-    itCmd('format(exp(2*pi*i), 2, delimiter->",", truncate->false)', '1,00');
-    itCmd('format(exp(2*pi*i), 2, delimiter->",", truncate->true)', '1');
+    itCmd('format(1.23456, 2, locale->"de")', '1,23');
+    itCmd('format(exp(2*pi*i), 2, locale->"de", truncate->false)', '1,00');
+    itCmd('format(exp(2*pi*i), 2, locale->"de", truncate->true)', '1');
+    itCmd('format(1234567.89, 1, locale->"de")', '1234567,9');
+    itCmd('format(123456.000, 2, truncate->false)', '123456.00');
 });
