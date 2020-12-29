c974568db8cbea4efdfb955ce81419da6f2df180
Michael Strobel <m.strobel@posteo.de>
2020-12-29 19:43:50 +0100

    eslint and jsconfig

---
+++ .eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+    env: {
+        browser: true,
+        es6: true,
+        node: true,
+    },
+    extends: "eslint:recommended",
+    globals: {
+        Atomics: "readonly",
+        SharedArrayBuffer: "readonly",
+        ClipperLib: true,
+        enableInlineVideo: true,
+        WebKitMutationObserver: true,
+    },
+    parserOptions: {
+        ecmaVersion: 2018,
+        sourceType: "module",
+    },
+    rules: {
+        // "no-undef": ["off"],
+        "no-unused-vars": ["off"],
+        "no-prototype-builtins": ["off"],
+    },
+};
