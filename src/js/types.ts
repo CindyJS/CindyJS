c6eb1c7dcc446bb5a4e88e1e9559d7e4bf90557c
Michael Strobel <m.strobel@posteo.de>
2022-11-03 14:20:08 +0100

    typescript: move types to own file

---
+++ src/js/types.ts
@@ -0,0 +1,17 @@
+export interface CindyType {
+  ctype: string;
+  value?: Record<string, any>;
+}
+
+export interface CindyNumber extends CindyType {
+  usage?: "Angle";
+  ctype: "number";
+  value: {
+    real: number;
+    imag: number;
+  };
+}
+
+export interface Nada {
+  ctype: string;
+}
