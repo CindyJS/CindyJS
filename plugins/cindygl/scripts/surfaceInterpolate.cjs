8ecb1c8b1896df095ff0cf8c445f2eb768802b51
benjaminSoelch <benjamin.soelch@tum.de>
2025-06-18 10:27:21 +0200

    reorder elements of coordinate powers
    + add functions 6th and 7th powers of coordinate

--- plugins/cindygl/scripts/surfaceInterpolate.cjs
+++ plugins/cindygl/scripts/surfaceInterpolate.cjs
@@ -21,60 +21,105 @@ maxDegreeForLen(pointCount):=(
   );
 );
 
+// TODO? is is possible to speed up computations by doing vectorized multiplications on sub-lists, 
+//   instead of element wise multiplications
+// -> needs compiler-support for eigther concatenation or flattening of mixed-lists
+// ? special function that returns true iff in compiled code to choose different algorithm for interpreted and compiled code
+
 squaredCoords(p):=(
   (
-    p_1*p_1,p_2*p_2,p_3*p_3,p_4*p_4,
-    p_1*p_2,p_1*p_3,p_1*p_4,
-    p_2*p_3,p_2*p_4,
-    p_3*p_4
+    p_1*p_1,p_1*p_2,p_1*p_3,p_1*p_4,
+    p_2*p_2,p_2*p_3,p_2*p_4,
+    p_3*p_3,p_3*p_4,
+    p_4*p_4
   )
 );
 cubedCoords(p):=(
   regional(s);
-  s = squaredCoords(p); // xx yy zz ww xy xz xw yz yw zw
+  s = squaredCoords(p); // xx xy xz xw yy yz yw zz zw ww
   (
-    p_1*s_1,p_2*s_2,p_3*s_3,p_4*s_4,
-    p_1*s_2,p_1*s_3,p_1*s_4,p_1*s_5,p_1*s_6,p_1*s_7,p_1*s_8,p_1*s_9,p_1*s_10,
-    p_2*s_3,p_2*s_4,p_2*s_8,p_2*s_9,p_2*s_10,
-    p_3*s_4,p_3*s_10
+    p_1*s_1,p_1*s_2,p_1*s_3,p_1*s_4,p_1*s_5,p_1*s_6,p_1*s_7,p_1*s_8,p_1*s_9,p_1*s_10,
+    p_2*s_5,p_2*s_6,p_2*s_7,p_2*s_8,p_2*s_9,p_2*s_10,
+    p_3*s_8,p_3*s_9,p_3*s_10,
+    p_4*s_10
   )
 );
 coordsP4(p):=(
   regional(s);
-  s = squaredCoords(p); // xx yy zz ww xy xz xw yz yw zw
-  // xxxx yyyy zzzz wwww
-  // xxyy xxzz xxww xxxy xxxz xxxw xxyz xxyw xxzw
-  // xyyy xyzz xyww xyyz xyyw xyzw  xzzz xzww xzzw xwww
-  // yyzz yyww yyyz yyyw yyzw
-  // yzzz yzww yzzw ywww
-  // zzww zzzw zwww
+  s = squaredCoords(p);
+  coordsP2toP4(s);
+);
+coordsP2toP4(s):=(
+  //s: xx xy xz xw yy yz yw zz zw ww
+  // xxxx xxxy xxxz xxxw xxyy xxyz xxyw xxzz xxzw xxww
+  // xyyy xyyz xyyw xyzz xyzw xyww xzzz xzzw xzww xwww
+  // yyyy yyyz yyyw yyzz yyzw yyww
+  // yzzz yzzw yzww ywww
+  // zzzz zzzw zzww zwww
+  // wwww
   (
-    s_1*s_1,s_2*s_2,s_3*s_3,s_4*s_4,
-    s_1*s_2,s_1*s_3,s_1*s_4,s_1*s_5,s_1*s_6,s_1*s_7,s_1*s_8,s_1*s_9,s_1*s_10,
-    s_5*s_2,s_5*s_3,s_5*s_4,s_5*s_8,s_5*s_9,s_5*s_10,s_6*s_3,s_6*s_4,s_6*s_10,s_7*s_4,
-    s_2*s_3,s_2*s_4,s_2*s_8,s_2*s_9,s_2*s_10,
-    s_8*s_3,s_8*s_4,s_8*s_10,s_9*s_4,
-    s_3*s_4,s_3*s_10,s_10*s_4
+    s_1*s_1,s_1*s_2,s_1*s_3,s_1*s_4,s_1*s_5,s_1*s_6,s_1*s_7,s_1*s_8,s_1*s_9,s_1*s_10,
+    s_2*s_5,s_2*s_6,s_2*s_7,s_2*s_8,s_2*s_9,s_2*s_10,s_3*s_8,s_3*s_9,s_3*s_10,s_4*s_10,
+    s_5*s_5,s_5*s_6,s_5*s_7,s_5*s_8,s_5*s_9,s_5*s_10,
+    s_6*s_8,s_6*s_9,s_6*s_10,s_7*s_10,
+    s_8*s_8,s_8*s_9,s_8*s_10,s_9*s_10,
+    s_10*s_10
   )
 );
 coordsP5(p):=(
   regional(s);
   s = coordsP4(p);
   (
-    s_1*p_1,s_2*p_2,s_3*p_3,s_4*p_4,
-    p_1*s_2,p_1*s_3,p_1*s_4,p_1*s_5,p_1*s_6,p_1*s_7,p_1*s_8,p_1*s_9,p_1*s_10,p_1*s_11,p_1*s_12,
-      p_1*s_13,p_1*s_14,p_1*s_15,p_1*s_16,p_1*s_17,p_1*s_18,p_1*s_19,p_1*s_20,p_1*s_21,p_1*s_22,
-      p_1*s_23,p_1*s_24,p_1*s_25,p_1*s_26,p_1*s_27,p_1*s_28,p_1*s_29,p_1*s_30,p_1*s_31,p_1*s_32,
-      p_1*s_33,p_1*s_34,p_1*s_35,
-    p_2*s_3,p_2*s_4,p_2*s_24,p_2*s_25,p_2*s_26,p_2*s_27,p_2*s_28,p_2*s_29,p_2*s_30,p_2*s_31,
+    p_1*s_1,p_1*s_2,p_1*s_3,p_1*s_4,p_1*s_5,p_1*s_6,p_1*s_7,p_1*s_8,p_1*s_9,p_1*s_10,p_1*s_11,p_1*s_12,
+      p_1*s_13,p_1*s_14,p_1*s_15,p_1*s_16,p_1*s_17,p_1*s_18,p_1*s_19,p_1*s_20,p_1*s_21,p_1*s_22,p_1*s_23,
+      p_1*s_24,p_1*s_25,p_1*s_26,p_1*s_27,p_1*s_28,p_1*s_29,p_1*s_30,p_1*s_31,p_1*s_32,p_1*s_33,p_1*s_34,p_1*s_35,
+    p_2*s_21,p_2*s_22,p_2*s_23,p_2*s_24,p_2*s_25,p_2*s_26,p_2*s_27,p_2*s_28,p_2*s_29,p_2*s_30,p_2*s_31,
       p_2*s_32,p_2*s_33,p_2*s_34,p_2*s_35,
-    p_3*s_4,p_3*s_34,p_3*s_35
+    p_3*s_32,p_3*s_33,p_3*s_34,p_3*s_35,
+    p_4*s_35
+  );
+);
+coordsP6(p):=(
+  regional(s,q);
+  s = squaredCoords(p);
+  q = coordsP2toP4(s);
+  (
+    s_1*q_1,s_1*q_2,s_1*q_3,s_1*q_4,s_1*q_5,s_1*q_6,s_1*q_7,s_1*q_8,s_1*q_9,s_1*q_10,s_1*q_11,s_1*q_12,
+      s_1*q_13,s_1*q_14,s_1*q_15,s_1*q_16,s_1*q_17,s_1*q_18,s_1*q_19,s_1*q_20,s_1*q_21,s_1*q_22,s_1*q_23,
+      s_1*q_24,s_1*q_25,s_1*q_26,s_1*q_27,s_1*q_28,s_1*q_29,s_1*q_30,s_1*q_31,s_1*q_32,s_1*q_33,s_1*q_34,
+      s_1*q_35,
+    s_2*q_21,s_2*q_22,s_2*q_23,s_2*q_24,s_2*q_25,s_2*q_26,s_2*q_27,s_2*q_28,s_2*q_29,s_2*q_30,s_2*q_31,
+      s_2*q_32,s_2*q_33,s_2*q_34,s_2*q_35,s_3*q_31,s_3*q_32,s_3*q_33,s_3*q_34,s_3*q_35,s_4*q_35,
+    s_5*q_21,s_5*q_22,s_5*q_23,s_5*q_24,s_5*q_25,s_5*q_26,s_5*q_27,
+      s_5*q_28,s_5*q_29,s_5*q_30,s_5*q_31,s_3*q_31,s_3*q_32,s_5*q_33,s_5*q_34,s_5*q_35,
+    s_6*q_31,s_6*q_32,s_6*q_33,s_6*q_34,s_6*q_35,s_7*q_35,
+    s_8*q_31,s_8*q_32,s_8*q_33,s_8*q_34,s_8*q_35,s_9*q_35,
+    s_10*q_35
+  );
+);
+coordsP7(p):=(
+  regional(s);
+  s = coordsP6(p);
+  // this is simply the calculation of three scalar-vector products
+  // implemented using single list constructor as compiled does not support concatenation
+  (
+    p_1*s_1,p_1*s_2,p_1*s_3,p_1*s_4,p_1*s_5,p_1*s_6,p_1*s_7,p_1*s_8,p_1*s_9,p_1*s_10,p_1*s_11,p_1*s_12,
+      p_1*s_13,p_1*s_14,p_1*s_15,p_1*s_16,p_1*s_17,p_1*s_18,p_1*s_19,p_1*s_20,p_1*s_21,p_1*s_22,p_1*s_23,
+      p_1*s_24,p_1*s_25,p_1*s_26,p_1*s_27,p_1*s_28,p_1*s_29,p_1*s_30,p_1*s_31,p_1*s_32,p_1*s_33,p_1*s_34,
+      p_1*s_35,p_1*s_36,p_1*s_37,p_1*s_38,p_1*s_39,p_1*s_40,p_1*s_41,p_1*s_42,p_1*s_43,p_1*s_44,p_1*s_45,
+      p_1*s_46,p_1*s_47,p_1*s_48,p_1*s_49,p_1*s_50,p_1*s_51,p_1*s_52,p_1*s_53,p_1*s_54,p_1*s_55,p_1*s_56,
+      p_1*s_57,p_1*s_58,p_1*s_59,p_1*s_60,p_1*s_61,p_1*s_62,p_1*s_63,p_1*s_64,p_1*s_65,p_1*s_66,p_1*s_67,
+      p_1*s_68,p_1*s_69,p_1*s_70,p_1*s_71,p_1*s_72,p_1*s_73,p_1*s_74,p_1*s_75,p_1*s_76,p_1*s_77,p_1*s_78,
+      p_1*s_79,p_1*s_80,p_1*s_81,p_1*s_82,p_1*s_83,p_1*s_84,
+    p_2*s_56,p_2*s_57,p_2*s_58,p_2*s_59,p_2*s_60,p_2*s_61,p_2*s_62,p_2*s_63,p_2*s_64,p_2*s_65,p_2*s_66,
+      p_2*s_67,p_2*s_68,p_2*s_69,p_2*s_70,p_2*s_71,p_2*s_72,p_2*s_73,p_2*s_74,p_2*s_75,p_2*s_76,p_2*s_77,
+      p_2*s_78,p_2*s_79,p_2*s_80,p_2*s_81,p_2*s_82,p_2*s_83,p_2*s_84,
+    p_3*s_79,p_3*s_80,p_3*s_81,p_3*s_82,p_3*s_83,p_3*s_84,
+    p_4*s_84
   );
 );
 // TODO? add functions for n-th power coordinates up to degree 16?
-// ? write code to auto-generate homogeneous power-function
-// 6 -> 4+2
-// 7 -> 6+1
+// ? write code to auto-generate general homogeneous power-function
 // 8 -> 4+4
 // 9 -> 8+1
 // 10 -> 8+2
@@ -88,10 +133,18 @@ coordsP5(p):=(
 
 coordsPnFct(n):=(
   if(n>4,
-    if(n>5,
-      cglLogError("unimplemented: coordsP"+text(n))
+    if(n>6,
+      if(n>7,
+        cglLogError("unimplemented: coordsP"+text(n))
+      ,
+        cglLazy(p,coordsP7(p))
+      )
     ,
-      cglLazy(p,coordsP5(p))
+      if(n==6,
+        cglLazy(p,coordsP6(p))
+      ,// n==5
+        cglLazy(p,coordsP5(p))
+      )
     );
   ,
     if(n>2,
@@ -132,7 +185,8 @@ pickKernelElement(K):=(
   )
 );
 
-// TODO? make degree modifier, defaulting to maxDegree
+// TODO is there s way to speed up to calculation?
+// the limiting factor seems to be the computation of matrix-kernel
 interpolateSurface(points):=(
   regional(maxDegree,samples,M,K,coordPFct);
   maxDegree = maxDegreeForLen(length(points));
